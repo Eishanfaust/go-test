04519309aad087fb962b0ce120b65b6de68789c4
Ishan Bhusal <ishansos59@gmail.com>
2026-01-20 11:48:49 +0545

    ever

---
+++ lib1/go.mod
@@ -0,0 +1,4 @@
+module github.com/Eishanfaust/go-tests/lib1
+
+go 1.25.5
+
---
+++ lib1/test1.go
@@ -0,0 +1,9 @@
+package lib1
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
